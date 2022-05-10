05c8d7bcbd28b66338cc175413bcf310f74b27e8
unknown <jtwest@free.fr>
2022-05-10 11:58:40 +0200

    add project

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.green}>
+      </View>
+      <View style={styles.red}>
+      </View>
+      <View style={styles.blue}>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
   },
+  green: {
+    flex: 3,
+    backgroundColor: 'green'
+  },
+  red: {
+    flex: 2,
+    backgroundColor: 'red'
+  },
+  blue: {
+    flex: 1,
+    backgroundColor: 'blue'
+  }
 });
